c1cf9f6e13a527088b3d6bd17867df221e9de906
P10r10 <asantiago@keemail.me>
2021-07-02 16:36:49 +0100

    ComputeTax

---
+++ ComputeTax.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ComputeTax{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("(0 - single, 1 - married jointly, 2 - married " +
+			"separately, 3 - head of household)");
+		System.out.print("Enter filing status: ");
+		int status = input.nextInt();
+
+		System.out.print(status);
+
+		input.close();
+	}
+}
