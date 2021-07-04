b860baca435419824febedf99a2e437acb39d2ee
P10r10 <asantiago@keemail.me>
2021-07-04 21:04:33 +0100

    ComputeTax

--- ComputeTax.java
+++ ComputeTax.java
@@ -10,9 +10,33 @@ public class ComputeTax{
 		int status = input.nextInt();
 		System.out.print("Enter taxable income: ");
 		double income = input.nextDouble();
+		double tax = 0;
 
-		System.out.print(status);
-		//This is a test
+		switch(status){
+			case 0:{
+				if (income <= 8350)
+					tax = income * 0.1;
+				else if (income <= 33950)
+					tax = 8350 * 0.1 + (income - 8350) * 0.15;
+				else if (income <= 82250)
+					tax = 8350 * 0.1 + (33950 - 8350) * 0.15 + (income - 33950) * 0.25;
+				else if (income <= 171550)
+					tax = 8350 * 0.1 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 +
+					(income - 82250) * 0.28;
+				else if (income <= 372950)
+					tax = 8350 * 0.1 + (33950 - 8350) * 0.15 + (82250 - 33951) * 0.25 +
+					(171550 - 82250) * 0.28 + (income - 171550) * 0.33;
+				else
+					tax = 8350 * 0.1 + (33950 - 8350) * 0.15 + (82250 - 33951) * 0.25 +
+					(171550 - 82250) * 0.28 + (372950 - 171550) * 0.33 + (income - 372950) * 0.35;
+				break;
+			}
+			case 1:
+			case 2:
+			case 3:
+		}
+
+		System.out.print(tax);
 
 		input.close();
 	}
