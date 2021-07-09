85be06d25357f719d7f18b7cbe94abe1a2686f1f
P10r10 <asantiago@keemail.me>
2021-07-09 15:59:19 +0100

    HeadsOrTails

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -6,12 +6,22 @@ public class HeadsOrTails{
 
 		System.out.println("Guess Heads or Tails (0 or 1): ");
 		int guess = input.nextInt();
+
 		System.out.print("Your guess was ");
 		if(guess == 0)
-			System.out.print("Heads!");
+			System.out.println("Heads!");
 		else if(guess == 1)
-			System.out.print("Tails!");
+			System.out.println("Tails!");
 		else
-			System.out.print("Unknown...");
+			System.out.println("Unknown...");
+
+		int appGuess = (int)(Math.random() * 2);
+		System.out.print("App guess was ");
+		if(appGuess == 0)
+			System.out.println("Heads!");
+		else if(appGuess == 1)
+			System.out.println("Tails!");
+		System.out.println("Your prediction was " +
+			(guess == appGuess ? "correct!" : "wrong!"));
 	}
 }
